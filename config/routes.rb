a25ee130c3bb0a71a91d9126ea0ff82bde79138b
Chris Cardle <cdcardle@gmail.com>
2018-09-12 12:01:25 -0400

    created users and updated css

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'application#home'
 
   get '/home' => 'application#home'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
